82aff72a0e35eda2d62cbc30919b19959aff7734
Kshitiz0504 <kshitizshukla05@gmail.com>
2025-04-20 12:37:54 +0530

    + useRef

--- react02/src/App.jsx
+++ react02/src/App.jsx
@@ -1,59 +1,120 @@
-// ErrorBoundary
 
-import React, { useState, useEffect } from 'react';
+// useRef -> 1.)Focusing on an Input Box
+//           2.) reference to a value , such that when you change the value, the component Doesnot re-render
 
-const App = () => {
-  return (
-    <div>
-      <ErrorBoundary>
-        <Card1/>
-      </ErrorBoundary>
-        <Card2/>
-    </div>
-  );
-};
 
-function Card1() {
 
-  throw newError("Error while rendering")
+  // 1.
+  // import { useRef, useState } from 'react'
+  // import './App.css'
 
-  return <div style={{background: "red", borderRadius: 20, padding: 20}}>
-    Hi there!
-  </div>
-}
+  // function App() {
+  //   const inputRef = useRef();
 
-function Card2() {
-  return <div style={{background: "red", borderRadius: 20, padding: 20}}>
-    Hello
-  </div>
-}
+  //   function focusOnInput() {
+  //     // document.getElementById("name").focus();     // can be done by this also but DOM manipulation ni krna react use krna h 
+  //     inputRef.current.focus();
+  //   }
 
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
+  //   return <div>
+  //     Sign up
+  //     <input ref={inputRef} type={"text"}></input>
+  //     <input type={"text"}></input>
+  //     <button onClick={focusOnInput}>Submit</button>
+  //   </div>
+  // }
 
-  static getDerivedStateFromError(error) {
-    return { hasError: true };
-  }
+  // export default App
 
-  componentDidCatch(error, info) {
-    console.error("Error caught:", error, info);
-  }
 
-  render() {
-    if (this.state.hasError) {
-      return <div style={{background: "red", borderRadius: 20, padding: 20}}>
-        Something went wrong
-        </div>
+  // 2.
+  import { useRef, useState } from 'react'
+  import './App.css'
+  // a clock with a start and stop button
+    function App() {
+      const[currentCount, setCurrentCount] = useState(1);
+      const timer = useRef();
+
+      function startClock() {
+        let value = setInterval(function() {
+          setCurrentCount(c => c+1);
+        }, 1000);
+        timer.current = value;
+      }
+
+      function stopClock() {
+        console.log(timer);
+        clearInterval(timer.current);
+      }
+
+      return <div>
+        {currentCount}
+        <br />
+        <button onClick={startClock}> Start </button>
+        <button onClick={stopClock}> Stop </button>
+      </div>
     }
 
-    return this.props.children
-  }
-}
 
-export default App;
+    export default App
+
+
+// ErrorBoundary
+
+// import React, { useState, useEffect } from 'react';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <ErrorBoundary>
+//         <Card1/>
+//       </ErrorBoundary>
+//         <Card2/>
+//     </div>
+//   );
+// };
+
+// function Card1() {
+
+//   throw newError("Error while rendering")
+
+//   return <div style={{background: "red", borderRadius: 20, padding: 20}}>
+//     Hi there!
+//   </div>
+// }
+
+// function Card2() {
+//   return <div style={{background: "red", borderRadius: 20, padding: 20}}>
+//     Hello
+//   </div>
+// }
+
+// class ErrorBoundary extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { hasError: false };
+//   }
+
+//   static getDerivedStateFromError(error) {
+//     return { hasError: true };
+//   }
+
+//   componentDidCatch(error, info) {
+//     console.error("Error caught:", error, info);
+//   }
+
+//   render() {
+//     if (this.state.hasError) {
+//       return <div style={{background: "red", borderRadius: 20, padding: 20}}>
+//         Something went wrong
+//         </div>
+//     }
+
+//     return this.props.children
+//   }
+// }
+
+// export default App;
 
 
 // Understanding Children
