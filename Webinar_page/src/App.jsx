3844e96e11108d2c35791931d60a289e3e44ae2c
Kshitiz0504 <kshitizshukla05@gmail.com>
2025-05-07 19:13:01 +0530

    Changes

---
+++ Webinar_page/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { Button } from './components/Buttons'
+import { Input } from './components/Input'
+import { Otp } from './components/Otp'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='h-screen bg-blue-700'>
+      <br /> <br /> <br /> <br />
+      <Otp number={20} />
+    </div>
+  )
+}
+
+export default App
