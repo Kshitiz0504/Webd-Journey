78f32bafd12ff6f97d9d16345da651df464ef629
Kshitiz0504 <kshitizshukla05@gmail.com>
2025-05-13 11:09:35 +0530

    Fixed API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { useEffect } from 'react'
 import './index.css'
 import Navbar from './components/Navbar/Navbar'
 import { Routes, Route } from 'react-router-dom'
@@ -7,6 +7,20 @@ import Home from './pages/Home/Home'
 import Footer from './components/Footer/Footer'
 
 function App() {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('/api/coingecko'); // calls your serverless function
+        const data = await res.json();
+        console.log(data);
+      } catch (err) {
+        console.error("Failed to fetch from serverless function:", err);
+      }
+    };
+  
+    fetchData();
+  }, []);
 
   return (
     <div className='app'>
