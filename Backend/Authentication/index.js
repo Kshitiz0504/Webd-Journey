9408c02ac34888decd7507831fc783c004d206c7
Kshitiz0504 <kshitizshukla05@gmail.com>
2025-05-20 23:36:02 +0530

    Sign In

--- Backend/Authentication/index.js
+++ Backend/Authentication/index.js
@@ -12,7 +12,7 @@ function generateToken() {
     let token = "";
     for (let i = 0; i < 32; i++) {
         // can use a simple function here
-        // we just want to generate a random string
+        // we just want to generate a random long string
         token += options[Math.floor(Math.random() * options.length)];
     }
     return token;
@@ -28,12 +28,49 @@ app.post("/signup", function (req, res) {
     })
 
     res.json({
-        message: "You are signed in"
+        message: "You are signed up"
     })
 
+    console.log(users)
 })
 
 app.post("/signin", function (req, res) {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let foundUser = null;
+
+    for(let i = 0; i<users.length; i++) {
+        if (users[i].username == username && users[i].password == password) {
+            foundUser = users[i];
+        }
+    }
+
+    if(foundUser) {
+        const token = generateToken();
+        foundUser.token = token; 
+        res.json({
+            token: token
+        }) 
+        } else {
+            res.status(403).send({
+                message: "Invalid username or password"
+            })
+        }
+
+        console.log(users)
+    
+
+    // either this or above one
+    // const user = users.find(function(u) {
+    //     if(u.username == username && u.password == password) {
+    //         return true;
+    //     }
+    //     else{
+    //         return false;
+    //     }
+    // })
 
 })
 
