14fcc62eef558179d7453f25e04f6a66274a07b3
Kshitiz0504 <kshitizshukla05@gmail.com>
2025-08-29 01:51:50 +0530

    Revision

--- Rolling_Up/src/App.jsx
+++ Rolling_Up/src/App.jsx
@@ -1,4 +1,10 @@
-// Rolling Up the State
+// Rolling Up the State ->
+// As our application grows, we might find that multiple components
+// need access to the same state
+// Instead of duplicating the state in each component
+// we can lift the state up to the
+// Lowest Common Ancestor(LCA) allowing
+// the common ancestor to manage it
 
 // import { useState } from 'react'
 // import './App.css'
@@ -14,6 +20,7 @@
 
 //   // bulbOn is a prop to the Bulb State component
 //   // setBulbOn is the prop to the ToggleBulbState component 
+
 //   return <div>
 //     <BulbState bulbOn={bulbOn} />
 //     <ToggleBulbState bulbOn={bulbOn} setBulbOn={setBulbOn} />
@@ -47,7 +54,9 @@
 // export default App
 
 
-// Prop Drilling -> It occurs when you need to pass data from a higher-level component down to a lower-level component that is several layers deep in the component tree.
+// Prop Drilling -> It occurs when you need to pass data
+//  from a higher-level component down to a lower-level component
+// that is several layers deep in the component tree.
 
 
 // import { useState } from 'react'
@@ -63,8 +72,7 @@
 
 // function Light({bulbOn, setBulbOn}) {
 
-
-//   return <div>
+//   return <div> 
 //     <LightBulb bulbOn={bulbOn} />
 //     <LightSwitch bulbOn={bulbOn} setBulbOn={setBulbOn} />
 //   </div>
@@ -102,7 +110,17 @@
 // export default App
 
 
-// Context API  -> The context API is a powerful feature in React that enables you to manage state across your application more effectively, especially when dealing with deeply nested components. It provides a way to share values(state, functions, etc.) between components without having to pass down props manually at every level.
+// Context API  -> The context API is a powerful feature
+//  in React that enables you to manage state
+//  across your application more effectively,
+//  especially when dealing with deeply nested components.
+//  It provides a way to share values(state, functions, etc.) 
+// between components without having to pass down props manually 
+// at every level.
+// Step 1 -> Define the context using createContext() [const xyz = createContext()]
+// Step 2 -> Provide the value that you want children to have using <xyz.Provider>
+// We need to give the value also
+// Step 3 -> Consume the context using UseContext(xyz)
 
 import { useState, createContext, useContext } from 'react'
 import './App.css'
@@ -116,7 +134,7 @@ function BulbProvider({ children }) {
     bulbOn: bulbOn,
     setBulbOn: setBulbOn
   }}>
-    { children}
+    { children }
   </BulbContext.Provider>
 }
 
